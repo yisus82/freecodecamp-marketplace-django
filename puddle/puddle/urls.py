1664078289d3136000d950b637fe93a58f8dcb9c
yisus82 <djalma8@gmail.com>
2023-03-15 09:17:11 +0100

    Item detail

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -2,10 +2,11 @@ from core.views import contact, index
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
