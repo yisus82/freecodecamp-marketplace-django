f81305aca0f2469a1dd096d60b45f7fefb5f3feb
yisus82 <djalma8@gmail.com>
2023-03-30 18:20:48 +0200

    Edit items

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404, redirect, render
 
-from .forms import NewItemForm
+from .forms import EditItemForm, NewItemForm
 from .models import Item
 
 
@@ -35,3 +35,16 @@ def delete(request, pk):
     item = get_object_or_404(Item, pk=pk, created_by=request.user)
     item.delete()
     return redirect('dashboard:index')
+
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('item:detail', pk=item.pk)
+    else:
+        form = EditItemForm(instance=item)
+    return render(request, 'item/form.html', {'form': form, 'title': 'Edit Item'})
