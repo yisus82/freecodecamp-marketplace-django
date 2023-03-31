732faf1058fcb9594f58359000a39623f8bbbd7b
yisus82 <djalma8@gmail.com>
2023-03-31 15:30:56 +0200

    Search items

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,8 +1,30 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import get_object_or_404, redirect, render
 
 from .forms import EditItemForm, NewItemForm
-from .models import Item
+from .models import Category, Item
+
+
+def items(request):
+    query = request.GET.get('query', '')
+    category_id = request.GET.get('category', 0)
+    categories = Category.objects.all()
+    items = Item.objects.filter(is_sold=False)
+
+    if category_id:
+        items = items.filter(category_id=category_id)
+
+    if query:
+        items = items.filter(Q(name__icontains=query) |
+                             Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id)
+    })
 
 
 def detail(request, pk):
