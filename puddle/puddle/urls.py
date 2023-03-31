412221ab21a922e4b63698e26eb7579c1af23139
yisus82 <djalma8@gmail.com>
2023-03-31 16:48:50 +0200

    Conversations

--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('items/', include('item.urls')),
     path('dashboard/', include('dashboard.urls')),
+    path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
